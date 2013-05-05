d538e628117ba0496ab15537a74ff38523f2e4f8
 randliu <buckler.liu@gmail.com>
2013-05-05 19:45:11 +0800

    version 0.1

---
+++ src/com/example/jydc/dummy/DummyContent.java
@@ -0,0 +1,61 @@
+package com.example.jydc.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+	/**
+	 * An array of sample (dummy) items.
+	 */
+	public static List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+	/**
+	 * A map of sample (dummy) items, by ID.
+	 */
+	public static Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+	static {
+		// Add 3 sample items.
+		addItem(new DummyItem("0", "0"));
+		addItem(new DummyItem("1", "1"));
+		addItem(new DummyItem("2", "2"));
+		addItem(new DummyItem("3", "3"));
+		addItem(new DummyItem("5", "5"));
+		addItem(new DummyItem("8", "8"));
+		addItem(new DummyItem("13", "13"));
+		addItem(new DummyItem("20", "20"));
+		addItem(new DummyItem("40", "40"));
+		}
+
+	private static void addItem(DummyItem item) {
+		ITEMS.add(item);
+		ITEM_MAP.put(item.id, item);
+	}
+
+	/**
+	 * A dummy item representing a piece of content.
+	 */
+	public static class DummyItem {
+		public String id;
+		public String content;
+
+		public DummyItem(String id, String content) {
+			this.id = id;
+			this.content = content;
+		}
+
+		@Override
+		public String toString() {
+			return content;
+		}
+	}
+}
